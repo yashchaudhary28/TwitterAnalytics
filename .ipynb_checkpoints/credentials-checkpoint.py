b7d8a8ae07b6befa76b5593e0fd915deaab59562
yashchaudhary28 <yashchaudhary.yc10@gmail.com>
2020-09-27 00:06:14 +0530

    First commit

---
+++ .ipynb_checkpoints/credentials-checkpoint.py
@@ -0,0 +1,82 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {
+    "ExecuteTime": {
+     "end_time": "2020-09-15T19:52:34.984437Z",
+     "start_time": "2020-09-15T19:52:34.980448Z"
+    }
+   },
+   "outputs": [],
+   "source": [
+    "#twitter token and keys \n",
+    "consumer_key= '5NU52PhIzB4CHFfc4cKuV1Ej5'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {
+    "ExecuteTime": {
+     "end_time": "2020-09-15T19:52:35.726541Z",
+     "start_time": "2020-09-15T19:52:35.723551Z"
+    }
+   },
+   "outputs": [],
+   "source": [
+    "consumer_secret= 'LxcwHJj8huVIFIR5hgWZGuW5snBMLoCp7HCOWTAEd0kNUcoD7t'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {
+    "ExecuteTime": {
+     "end_time": "2020-09-15T19:52:36.397078Z",
+     "start_time": "2020-09-15T19:52:36.393089Z"
+    }
+   },
+   "outputs": [],
+   "source": [
+    "access_token= '770301785349042176-n1iOSViYBU9ql9NmoPfKqxMFcgoP5XA'"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {
+    "ExecuteTime": {
+     "end_time": "2020-09-15T19:52:37.011435Z",
+     "start_time": "2020-09-15T19:52:37.007446Z"
+    }
+   },
+   "outputs": [],
+   "source": [
+    "access_token_secret= 'HkiL4BL38UuXHHAY96aWa77d0P8CwKG8Ob1WwldFasWBF'"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
