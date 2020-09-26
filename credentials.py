97bb040ac228bd81339ed32aa291304e7910a49e
Yash Chaudhary <55957378+yashchaudhary28@users.noreply.github.com>
2020-09-27 00:07:44 +0530

    Delete credentials.py

--- credentials.py
+++
@@ -1,8 +0,0 @@
-#twitter token and keys
-consumer_key= '5NU52PhIzB4CHFfc4cKuV1Ej5'
-
-consumer_secret= 'LxcwHJj8huVIFIR5hgWZGuW5snBMLoCp7HCOWTAEd0kNUcoD7t'
-
-access_token= '770301785349042176-n1iOSViYBU9ql9NmoPfKqxMFcgoP5XA'
-
-access_token_secret= 'HkiL4BL38UuXHHAY96aWa77d0P8CwKG8Ob1WwldFasWBF'
